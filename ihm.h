e5f810797f01e69415eb9b10c64de3fc456b638a
Amaury Graillat <amaury@amoweb.fr>
2014-01-02 11:27:30 +0000

    Comments and refactoring.

--- ihm.h
+++ ihm.h
@@ -1,7 +1,17 @@
 #ifndef IHM_C
 #define IHM_C
 
-char IHMuserEntry();
+/**
+ * Initialization of IHM
+ **/
 void IHMinit();
+
+/**
+ * Indicate the mouse state
+ * @require : call IHMinit
+ * @return 'L', 'M', 'R', if left, middle, or
+ * right button of the mouse activated, or 0
+ **/
+char IHMuserEntry();
 
 #endif
--- midi.h
+++ midi.h
@@ -12,13 +12,46 @@ struct Note {
         long t;
 };
 
+/**
+ * Initialisation of midi I/O
+ **/
 bool midiInit();
+
+/**
+ * Open the devices
+ * @param inId  input device ID
+ * @param outId output device ID
+ **/
 void midiOpenDevice(int inId, int outId);
+
+/**
+ * @return true if the a new score was received
+ **/
 bool inputAvailable();
+
+/**
+ * Send a score
+ **/
 void midiNoteOn(int note, int velocity);
+
+/**
+ * Stop a score
+ **/
 void midiNoteOff(int note);
+
+/**
+ * Read the last midi event
+ **/
 void midiReadLast(struct Note *note);
+
+/**
+ * Write a midi event
+ **/
 void midiWrite(struct Note *note);
+
+/**
+ * Flush the midi input
+ **/
 void midiFlush();
 
 #endif
